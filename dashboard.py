71670202a1417109d1bffd36954410e16c3fe7ce
Michael Slice <mjsens523@gmail.com>
2023-05-25 22:59:42 -0500

    Added Settings Button, and Equal Display Code

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import yfinance as yf
+import os
 
 def display_stock_prices():
     output = "Displaying stock prices..."
@@ -21,6 +22,11 @@ def display_company_data():
 def display_stock_charts():
     output = "Displaying stock charts..."
     label_output.config(text=output)
+
+# Function to open the settings file
+def open_settings_file():
+    # Open the settings file
+    os.system("settings.py")  # Replace "notepad.exe" with the appropriate program for your file type
 
 def add_stock():
     symbol = entry_stock.get().upper()[:4]  # Limit to maximum 4 characters
@@ -45,23 +51,15 @@ def add_stock():
         remove_button.pack(side=tk.LEFT, padx=5)
 
         # Create the "Details" button
-        details_button = ttk.Button(stock_entry_frame, text="Details", command=lambda s=symbol, f=stock_entry_frame: display_details(s, f))
+        details_button = ttk.Button(stock_entry_frame, text="Details")
         details_button.pack(side=tk.LEFT, padx=5)
 
-def display_details(symbol, stock_entry_frame):
-    # Retrieve stock information using yfinance
-    stock_data = yf.download(symbol, period='1d')
+        # Set the column to zero for all widgets in stock_entry_frame
+        for child in stock_entry_frame.winfo_children():
+            child.pack(side=tk.LEFT)
 
-    # Create a new frame for displaying the stock data
-    details_frame = ttk.Frame(stock_entry_frame)
-    details_frame.pack(side=tk.LEFT, padx=5)
-
-    # Create labels to display the stock data horizontally
-    for key, value in stock_data.iloc[0].items():
-        if key in ['Open', 'High', 'Low', 'Close', 'Adj Close']:
-            value = round(value, 2)
-        stock_label = ttk.Label(details_frame, text=f"{key}: {value}", width=20)
-        stock_label.pack(side=tk.LEFT, padx=5)
+        # Increment the row index for the next stock entry
+        stock_frame.grid_rowconfigure(stock_frame.grid_size()[1] + 1, weight=1)
 
 def remove_stock(stock_entry_frame):
     stock_entry_frame.destroy()
@@ -82,17 +80,22 @@ button_option_prices = ttk.Button(window, text="Option Prices", command=display_
 button_screener = ttk.Button(window, text="Screener", command=display_screener)
 button_company_data = ttk.Button(window, text="Company Data", command=display_company_data)
 button_stock_charts = ttk.Button(window, text="Stock Charts", command=display_stock_charts)
+button_settings = ttk.Button(window, text="!", command=open_settings_file)
+
+# Assign buttons to the same sizing group
+window.grid_columnconfigure((0, 1, 2, 3, 4), weight=1, uniform="equal")
 
 # Place buttons in the window using grid layout
-button_stock_prices.grid(row=0, column=0, padx=10, pady=10)
-button_option_prices.grid(row=0, column=1, padx=10, pady=10)
-button_screener.grid(row=0, column=2, padx=10, pady=10)
-button_company_data.grid(row=0, column=3, padx=10, pady=10)
-button_stock_charts.grid(row=0, column=4, padx=10, pady=10)
+button_stock_prices.grid(row=0, column=0, padx=10, pady=10, sticky="we")
+button_option_prices.grid(row=0, column=1, padx=10, pady=10, sticky="we")
+button_screener.grid(row=0, column=2, padx=10, pady=10, sticky="we")
+button_company_data.grid(row=0, column=3, padx=10, pady=10, sticky="we")
+button_stock_charts.grid(row=0, column=4, padx=10, pady=10, sticky="we")
+button_settings.place(relx=1.0, rely=1.0, anchor='se', x=-10, y=-10, width=30, height=30)
 
 # Create a frame to hold the input elements
 input_frame = ttk.Frame(window)
-input_frame.grid(row=1, column=0, padx=10, pady=10, sticky="e")
+input_frame.grid(row=1, column=0, padx=10, pady=10, sticky="w")
 
 # Create entry field to enter stock symbols
 entry_stock = ttk.Entry(input_frame, width=15)  # Adjust the width as needed
@@ -110,6 +113,10 @@ stock_frame.grid(row=2, column=0, padx=10, pady=10, sticky="w")
 
 # Create a label to display the output
 label_output = ttk.Label(window, wraplength=500)
-label_output.grid(row=3, column=0, columnspan=5, padx=10, pady=10, sticky="w")
+label_output.grid(row=1, column=0, columnspan=5, padx=10, pady=10, sticky="w")
+
+# Create a label to display the output
+label_output = ttk.Label(window, wraplength=500)
+label_output.grid(row=1, column=0, columnspan=5, padx=10, pady=10, sticky="w")
 
 window.mainloop()
